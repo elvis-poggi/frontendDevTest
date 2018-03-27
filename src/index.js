d5012c07a90365d00b4f78090147bfda66e65378
elvis <poggi.elvis@gmail.com>
2018-03-27 17:54:08 +0200

    removes serviceWorkers

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
 import { ParallaxProvider } from 'react-scroll-parallax'
 
 ReactDOM.render(
@@ -10,4 +9,3 @@ ReactDOM.render(
     <App />
   </ParallaxProvider>,
 document.getElementById('root'))
-registerServiceWorker()
