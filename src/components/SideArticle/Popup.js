b782d70e2cad36fbd1ff7f73513fa1b69f0bc5a8
elvis <poggi.elvis@gmail.com>
2018-03-27 12:05:59 +0200

    adds animation on gallery while opening popup

--- src/components/SideArticle/Popup.js
+++ src/components/SideArticle/Popup.js
@@ -1,16 +1,17 @@
 import React from 'react'
-// import Debug from '../../components/Debug'
 
 const Popup = (props = {}) => {
-  const { data = {}, closePopup } = props
-  const { images, heading } = data
+  const { data = {}, closePopup, isOpen } = props
+  const { images = [], heading } = data
+  const openClass = isOpen ? 'open' : ''
+
   return (
-    <div className='popup'>
+    <div className={`popup ${openClass}`}>
       <div className='popup_inner'>
         <div className='header'>
           <p>{heading}</p>
         </div>
-        <div className='gallery'>
+        <div className={`gallery ${openClass}`}>
           {images.map((image) => <img src={image.url} alt={image.type} key={image.id} />)}
         </div>
         <button onClick={closePopup}>click here</button>
