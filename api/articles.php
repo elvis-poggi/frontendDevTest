b455802dc40b936f411ee57f092056638d5b9c8a
elvis <poggi.elvis@gmail.com>
2018-03-28 10:15:50 +0200

    proxy article api rest call

--- api/articles.php
+++ api/articles.php
@@ -3,11 +3,16 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset-UTF-8");
 
 // TODO
-// - fetch from http://18.195.225.57/articles.php
 // - remove unnecessary fields from JSON
-// - serve filtered object as JSON
 
-$result = array();
-echo json_encode($result);
+$url = "http://18.195.225.57/articles.php";
+
+$client = curl_init($url);
+curl_setopt($client,true,true);
+$response = curl_exec($client);
+
+$result = json_decode($response);
+
+echo $result->data; 
 
 ?>
